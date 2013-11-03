9f6cbca564150f4fd811ec9ba12776a2c164d1b5
Brian Lee <brian@mutualmind.com>
2013-11-03 11:24:55 -0600

    improved trampoline function

--- fptools.py
+++ fptools.py
@@ -1,13 +1,6 @@
 from types import GeneratorType
 from itertools import izip
 
-
-"""
-Return the size of the shortest list in a list of lists
-"""
-def shortest(lists):
-    return min(lists, key=len)
-
 """
 Zip n iterables into one list, calling func on the lists as a tuple
 """
@@ -16,11 +9,11 @@ def zipWith(func, *lists):
         yield func(*val)
 
 """
-Cycle through the values of _list forever
+Cycle through the values of lst forever
 """
-def cycle(_list):
+def cycle(lst):
     while True:
-        for item in _list:
+        for item in lst:
             yield item
 
 """
@@ -40,12 +33,12 @@ def count(start=0, step=1):
         now = now + start + step
 
 """
-Partition an iterable _list into a list of lists, each of
+Partition an iterable lst into a list of lists, each of
 length 0-size
 """
-def partition(_list, size):
-    for item, pos in izip(_list, count(0, size)):
-        items = _list[pos:pos + size]
+def partition(lst, size):
+    for item, pos in izip(lst, count(0, size)):
+        items = lst[pos:pos + size]
         if items:
             yield items
         else:
@@ -54,41 +47,47 @@ def partition(_list, size):
 """
 Yield the first element as a single element list or empty list
 """
-def head(_list):
-    return next(iter(_list))
+def head(lst):
+    return next(iter(lst))
 
 """
 Yield 2nd element and beyond
 """
-def tail(_list):
-    items = iter(_list)
+def tail(lst):
+    items = iter(lst)
     next(items)
     for item in items:
         yield item
 
 """
 Yield everything except the last element
+
+I haven't figured out how to do this yet
 """
-def init(_list):
+def init(lst):
     raise NotImplementedError
 
 """
 Return the last item.
 """
-def last(_list):
+def last(lst):
     lt = None
-    for item in _list:
+    for item in lst:
         lt = item
     return lt
 
 """
-Obligatory recursive fibonacci
+The Trampoline pattern. Takes a generator that yields itself from next()
+Allows recursive algorithms without exceeding max recursion depth.
 """
-def fibonacci(count, _current=0, _next=1):
-    if count <= 1:
-        yield _current
-    else:
-        yield fibonacci(count - 1, _next, _current + _next)
+def trampoline(func, *args, **kwargs):
+    gen = func(*args, **kwargs)
+    try:
+        while isinstance(gen, GeneratorType):
+            gen = gen.next()
+    except StopIteration:
+        pass
+    return gen
 
 """
 Recursive Euclid's algorithm for finding GCD
@@ -99,16 +98,9 @@ def gcd(a, b):
     else:
         yield gcd(b, a % b)
 
-"""
-The Trampoline pattern. Takes a generator that yields itself from next()
-Allows recursive algorithms without exceeding max recursion depth.
-"""
-def trampoline(gen, *args, **kwargs):
-    func = gen(*args, **kwargs)
-    while isinstance(func, GeneratorType):
-        try:
-            func = func.next()
-        except StopIteration:
-            break
-    return func
+def fib(count, crnt=0, nxt=1):
+    if count <= 1:
+        yield crnt
+    else:
+        yield fib(count - 1, nxt, crnt + nxt)
 
